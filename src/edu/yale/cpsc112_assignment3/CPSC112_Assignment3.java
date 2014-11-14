c3f9601ddf4fe6141acd563feb5e6519d730c455
kaityhsieh <khsieh26@gmail.com>
2014-11-14 01:58:10 -0500

    Finished Part 2

--- src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
+++ src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
@@ -56,7 +56,64 @@ public class CPSC112_Assignment3 {
 
   }
 
+
   public static boolean isGuessValid(String input) {
+    // Part 2 code goes here
+		  
+		  try {
+			  
+			  //divide the input into 4 individual strings 
+			  String firstDigit = new String(input.substring(0,1));
+			  String secondDigit = new String(input.substring(1,2));
+			  String thirdDigit = new String(input.substring(2,3));
+			  String fourthDigit = new String(input.substring(3,4));
+			  
+			  //convert the strings to int
+			  int finalFirstDigit = Integer.parseInt(firstDigit);
+			  int finalSecondDigit = Integer.parseInt(secondDigit);
+			  int finalThirdDigit = Integer.parseInt(thirdDigit);
+			  int finalFourthDigit = Integer.parseInt(fourthDigit);
+			  
+			  //checking if all the digits are < 7 
+			  if (finalFirstDigit > 7 || finalSecondDigit > 7 || finalThirdDigit > 7 || finalFourthDigit > 7) {
+				  System.out.println("Input must be a 4-digit number with digits between 1 and 7.");
+				  return false; 
+			  }
+			 
+			  //checking if all the digits are > 1
+			  else if (finalFirstDigit < 1 || finalSecondDigit < 1 || finalThirdDigit < 1 || finalFourthDigit < 1) {
+				  System.out.println("Input must be a 4-digit number with digits between 1 and 7.");
+				  return false; 
+			  }
+				  
+			  //checking if any of the digits repeat
+			  else if (finalFirstDigit == finalSecondDigit || finalFirstDigit == finalThirdDigit || finalFirstDigit == finalFourthDigit){
+				  System.out.println("Input must be a 4-digit number with digits between 1 and 7.");
+				  return false;
+			  }
+			  else if (finalSecondDigit == finalThirdDigit || finalSecondDigit == finalFourthDigit){
+				  System.out.println("Input must be a 4-digit number with digits between 1 and 7.");
+				  return false; 
+			  }
+			  else if (finalThirdDigit == finalFourthDigit){
+				  System.out.println("Input must be a 4-digit number with digits between 1 and 7.");
+				  return false; 
+			  }
+			  else {
+				  //checking if the input is 4 digits 
+				  if (input.length() != 4) {
+					  System.out.println("Input must be a 4-digit number with digits between 1 and 7.");
+					  return false; 
+				  }
+			  }
+			  	return true;
+		  	}	
+		  
+	 catch (Exception e)
+	 {
+		 System.out.println("Input must be a 4-digit number with digits between 1 and 7.");
+		 return false;
+	 }
   }
 
 
